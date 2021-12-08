0d58e789c25b409c36c1d104e6cfc7cea831f499
Steve Brownlee <chortlehoort@gmail.com>
2021-12-08 16:27:51 -0600

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+/*
+  Write a function that returns an array of strings. The strings in
+  the array should be extracted from the location property of 
+  each object in the array provided.
+  Below are two arrays that can be used.
+*/
+
+
+
+const treasures = [
+    { id: 1, prize: "Bronze coin", location: "Australia" },
+    { id: 2, prize: "Sapphire pendant", location: "Egypt" },
+    { id: 3, prize: "Platinum comb", location: "Norway" },
+    { id: 4, prize: "Jade elephant", location: "Thailand" }
+]
+
+const books = [
+    { id: 1, prize: "A Brief History of Time", location: "United States" },
+    { id: 2, prize: "Anna Karenina", location: "Russia" },
+    { id: 3, prize: "Travelling with Djinns", location: "Sudan" },
+    { id: 4, prize: "Ferdydurke", location: "Poland" }
+]
+

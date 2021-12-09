649b7ed33f81c075357c1a8f446d45ee04e0bebd
Steve Brownlee <stevebrownlee@users.noreply.github.com>
2021-12-09 11:29:17 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,9 @@
   Write a function that returns an array of strings. The strings in
   the array should be extracted from the location property of 
   each object in the array provided.
-  Below are two arrays that can be used.
+  
+  Below are two arrays that can be used. Your function should be 
+  able to work for either array.
 */
 
 
